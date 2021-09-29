b31814d0b21cfdc6e67711751b27c3eef0f14a6c
HoDominic <dominic.ho97@gmail.com>
2021-09-29 16:43:19 +0200

    Added GET documents request to CodaRepository

--- Ex01/Ex01/Repositories/CodaRepository.cs
+++ Ex01/Ex01/Repositories/CodaRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Ex01.Models;
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -30,5 +31,53 @@ namespace Ex01.Repositories
             return client;
 
         }
+
+
+
+
+        //API documentation of this request: https://coda.io/developers/apis/v1#operation/listDocs
+
+        //Paste this in POSTMAN : https://coda.io/apis/v1/docs
+
+
+        //GET list of Documents
+        public static async Task<List<CodaDocument>> GetDocumentsAsync()
+        {
+            using (HttpClient client = await GetClient())
+            {
+
+                //https://coda.io/apis/v1/docs
+
+                String url = _BASEURL + "/docs";
+                string json = await client.GetStringAsync(url);
+
+                try
+                {
+
+
+                    //json convert to list
+                    if (json != null)
+                    {
+                        //json --> List Documents
+                        var documents = JsonConvert.DeserializeObject<CodaDocument>(json);
+                        return documents.CodaDocuments;
+
+                    }
+                    else
+
+                    {
+                        return null;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+
+            }
+
+
+        }
+
     }
 }
