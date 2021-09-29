4d8e580cabf7d75090fa835c1c7c40bc08bb633b
HoDominic <dominic.ho97@gmail.com>
2021-09-29 16:40:51 +0200

    Added Token authentication and HttpClient

--- Ex01/Ex01/Repositories/CodaRepository.cs
+++ Ex01/Ex01/Repositories/CodaRepository.cs
@@ -1,10 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Ex01.Repositories
 {
     public class CodaRepository
     {
+
+        //define base URL
+        private const String _BASEURL = "https://coda.io/apis/v1";
+        private const String _Token = "f69b44ef-0585-4ce0-88ac-33dc3e81829d";
+
+
+
+
+        //prepare HttpClient
+        private static async Task<HttpClient> GetClient()
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+            //BEARER TOKEN 
+            client.DefaultRequestHeaders.Authorization =
+            new AuthenticationHeaderValue("Bearer", "f69b44ef-0585-4ce0-88ac-33dc3e81829d");
+
+            return client;
+
+        }
     }
 }
